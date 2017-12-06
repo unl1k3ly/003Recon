3ebbedb3709735079496ce30d8de54aaef2a0577
unl1k3ly <vinicius777@gmail.com>
2017-12-07 06:31:05 +1100

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -4,7 +4,7 @@
     
     home_dir=$(pwd)
 
-    output_dir="output"
+    output_dir="/root/aquatone/$@/003Recon"
     tools_dir="tools"
     payloads_dir="payloads"
     dependencies_dir="dependencies"  
@@ -15,7 +15,7 @@
     domains_file="$output_dir/$@/domains.txt"
     crlf_file="$output_dir/$@/crlf.txt"
     open_redirects_file="$output_dir/$@/open_redirects.txt"
-    nmap_scan_file="$output_dir/$@/nmap_scans.txt"
+    nmap_scan_file="$output_dir/$@/$@_nmap"
     wordpress_file="$output_dir/$@/wordpress_sites.txt"
     headers_file="$output_dir/$@/sensitive_headers.txt"
     subdomain_take_over_file="$output_dir/$@/sub_take_over.txt"
@@ -29,11 +29,11 @@
     headers_payload_file="$payloads_dir/sensitive_headers.txt"
     open_redirect_payload_file="$payloads_dir/open_redirects.txt"
     
-    wpscan_location="dependencies/wpscan"
+    #wpscan_location="dependencies/wpscan"
     url_extractor_location="dependencies/relative-url-extractor"
     sublister_location="dependencies/sublister"
-    webscreenshot_location="dependencies/webscreenshot"
-    nmap_location="dependencies/nmap"
+    #webscreenshot_location="dependencies/webscreenshot"
+    nmap_location="/bin/nmap"
 
 
     cd $home_dir/$output_dir;
@@ -56,8 +56,8 @@
     $tools_dir/javascript_files_link_extractor.sh $javascript_files_file $javascript_extracted_urls $url_extractor_location/extract.rb;
     python $webscreenshot_location/webscreenshot.py -i $domains_file -o $screenshots_dir;
     python $tools_dir/wordpress_check.py $domains_file $wordpress_file;
-    $wpscan_location/wpscan.rb --update;
-    $tools_dir/wpscan_domains.sh $wordpress_file;
+    #$wpscan_location/wpscan.rb --update;
+    #$tools_dir/wpscan_domains.sh $wordpress_file;
     $tools_dir/nmap_scan.sh $domains_file $nmap_scan_file $nmap_location;
     
     printf "\n -- $@ Finished -- \n"
